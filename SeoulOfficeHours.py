32371d5fd0f66e83d5450f4fb51d718e247a3322
DaengMa <qwq713@hotmail.com>
2022-03-12 23:39:56 +0900

    [20220313] describe_instances에 NextToken 코드 수정

--- SeoulOfficeHours.py
+++ SeoulOfficeHours.py
@@ -9,44 +9,58 @@ class SeoulOfficeHours:
     def __init__(self):
         self.ec2_client = boto3.client('ec2')
 
-    def instance_ids(self, res_describe_instances) -> List[str]:
+    def instance_ids(self, filter) -> List[str]:
         """
         describe_instances 의 Response 값을 기반으로 instance_id 정보만을 List형태로 출력한다.
         """
         result = []
-        reservations = res_describe_instances['Reservations']
+        
+        resp = self.ec2_client.describe_instances(Filters=filter)
+        next_token = resp.get("NextToken", False)
+        reservations = resp['Reservations']
+        
         for reservation in reservations:
             instances = reservation['Instances']
             for instance in instances:
                 result.append(instance['InstanceId'])
+
+        while next_token:
+            resp = self.ec2_client.describe_instances(Filters=filter,NextToken=next_token)
+            next_token = resp.get("NextToken", False)
+            reservations = resp['Reservations']
+            for reservation in reservations:
+                instances = reservation['Instances']
+                for instance in instances:
+                    result.append(instance['InstanceId'])
         return result
 
     def start(self, tgt_tag_filter: List[Dict[str, str]]) -> str:
         """
         SeoulOfficeHours EC2 START
         """
-        instance_ids = self.instance_ids(
-            self.ec2_client.describe_instances(Filters=tgt_tag_filter))
+
+        instance_ids = []
+        instance_ids = self.instance_ids(tgt_tag_filter)
 
         if not instance_ids:
             return "There are no instances to start."
 
-        response = self.ec2_client.start_instances(InstanceIds=instance_ids,
-                                                   DryRun=False)
+        response=self.ec2_client.start_instances(InstanceIds = instance_ids,
+                                                   DryRun = False)
         return "START INSTANCES\n"+"\n".join([instance['InstanceId'] for instance in response['StartingInstances']])
 
     def stop(self, tgt_tag_filter: List[Dict[str, str]]) -> str:
         """
         SeoulOfficeHours EC2 OFF  ( tgt_tag_filter 정보를 통해 대상을 필터링한다.)
         """
-        instance_ids = self.instance_ids(
+        instance_ids=self.instance_ids(
             self.ec2_client.describe_instances(Filters=tgt_tag_filter))
 
         if not instance_ids:
             return "There are no instances to stop."
 
-        response = self.ec2_client.stop_instances(InstanceIds=instance_ids,
-                                                  DryRun=False)
+        response=self.ec2_client.stop_instances(InstanceIds = instance_ids,
+                                                  DryRun = False)
         return "STOP INSTANCES\n"+"\n".join([instance['InstanceId'] for instance in response['StoppingInstances']])
 
 
@@ -55,30 +69,30 @@ def lambda_handler(event, context):
     cloudwatch로부터 전달된 event 명을 기반으로
     seoul_office_start 혹은 seoul_office_stop 을 실행한다.
     """
-    TAG_KEY: str = "Schedule"
-    TAG_VAL_DEFAULT: str = "SEOUL-OFFICE-HOURS"
+    TAG_KEY: str="Schedule"
+    TAG_VAL_DEFAULT: str="SEOUL-OFFICE-HOURS"
 
-    _, event_name = event['resources'][0].split("/")
+    _, event_name=event['resources'][0].split("/")
 
     if event_name == 'seoul_office_start':
         # start 이벤트는 {"Key":"Schedule" , "Value":"SEOUL_OFFICE_HOURS*"} 태그값으로 대상을 필터링한다.
-        tag_filter = [{'Name': 'tag:Schedule',
+        tag_filter=[{'Name': 'tag:Schedule',
                        'Values': [TAG_VAL_DEFAULT+'*']}]
-        msg: str = SeoulOfficeHours().start(tgt_tag_filter=tag_filter)
+        msg: str=SeoulOfficeHours().start(tgt_tag_filter = tag_filter)
 
     elif event_name == 'seoul_office_stop':
-        hours: str = datetime.datetime.now().strftime('%H')
-        tag_val: str = TAG_VAL_DEFAULT if hours == "18" else TAG_VAL_DEFAULT + \
+        hours: str=datetime.datetime.now().strftime('%H')
+        tag_val: str=TAG_VAL_DEFAULT if hours == "18" else TAG_VAL_DEFAULT + \
             f"-{hours}"
 
-        tag_filter = [{'Name': 'tag:Schedule',
+        tag_filter=[{'Name': 'tag:Schedule',
                        'Values': [tag_val]}]
-        msg: str = SeoulOfficeHours().stop(tgt_tag_filter=tag_filter)
+        msg: str=SeoulOfficeHours().stop(tgt_tag_filter = tag_filter)
 
     else:
-        msg = """
+        msg="""
         The event name could not be found.
         Please check the event name.
         """
-    now_datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M-%S')
+    now_datetime=datetime.datetime.now().strftime('%Y-%m-%d %H:%M-%S')
     print(f"[{now_datetime}]", msg)
--- SeoulOfficeHoursTagManager.py
+++ SeoulOfficeHoursTagManager.py
@@ -42,20 +42,7 @@ class SeoulOfficeHoursTagManager:
             pprint(e)
             return 0
 
-    def filter_tgt_instance_ids(self) -> List[str]:
-        """
-        EC2 목록 중 Schedule 태그값이 EXCEPTION- 으로 시작하는 대상을 가져온다.
-        이중 예외기간이 지난 인스턴스의 ID 목록을 리스트 형식으로 리턴한다.
-        """
-        tgt_instance_ids = []
-        tgt_tag_filter = [{"Name": 'tag:'+self.TAG_KEY,
-                           "Values": [self.TAG_VAL_EXCEPTION_DEFAULT]}]
-
-        # Schedule 태그값이 EXCEPTION- 으로 시작하는 EC2 목록을 가져온다 by tgt_tag_filter
-        reservations = self.ec2_client.describe_instances(
-            Filters=tgt_tag_filter)
-        reservations = reservations.get('Reservations', [])
-        
+    def append_tgt_instances(self, tgt_instance_ids, reservations):
         for reservation in reservations:
             instances = reservation['Instances']
             for instance in instances:
@@ -67,6 +54,29 @@ class SeoulOfficeHoursTagManager:
                 # Schedule Tag값이 존재하고 & EXCEPTION 날짜 이후인 EC2의 ID값을 가져온다.
                 if schedule_tag and self.comp_nowdate(schedule_tag[0]['Value']):
                     tgt_instance_ids.append(instance["InstanceId"])
+
+    def filter_tgt_instance_ids(self) -> List[str]:
+        """
+        EC2 목록 중 Schedule 태그값이 EXCEPTION- 으로 시작하는 대상을 가져온다.
+        이중 예외기간이 지난 인스턴스의 ID 목록을 리스트 형식으로 리턴한다.
+        """
+
+        tgt_instance_ids = []
+        tgt_tag_filter = [{"Name": 'tag:'+self.TAG_KEY,
+                           "Values": [self.TAG_VAL_EXCEPTION_DEFAULT]}]
+
+        # Schedule 태그값이 EXCEPTION- 으로 시작하는 EC2 목록을 가져온다 by tgt_tag_filter
+        reservations = self.ec2_client.describe_instances(
+            Filters=tgt_tag_filter)
+        next_token = reservations.get('NextToken', False)
+        reservations = reservations.get('Reservations', [])
+        self.append_tgt_instances(tgt_instance_ids,reservations)
+
+        while next_token:
+            reservations = self.ec2_client.describe_instances(Filters=tgt_tag_filter,NextToken = next_token)
+            next_token = reservations.get('NextToken', False)
+            reservations = reservations.get('Reservations', [])
+            self.append_tgt_instances(tgt_instance_ids,reservations)
         return tgt_instance_ids
 
     def start(self) -> Any:
